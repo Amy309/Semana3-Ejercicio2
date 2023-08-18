c3b83d625af0b2e1d7d2d1f149c0b412abea8a41
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-08-17 18:10:35 -0600

    Ejercicio2

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ejercicio 2\n");
+
+        string nombre = "AMY NOELIA";
+        int longitud = nombre.Length;
+
+        char[] arregloNombre = new char[longitud];
+
+        for (int i = 0; i < longitud; i++)
+        {
+            arregloNombre[i] = nombre[i];
+            Console.WriteLine("Valor en posición " + i + ": " + arregloNombre[i]);
+        }
+
+        Console.WriteLine("");
+
+        Console.WriteLine("Tamaño del arreglo: " + longitud);
+    }
+}
+
